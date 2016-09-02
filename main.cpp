321ac738bc0a9bb9174f0b52d5a41fd8898be27b
dhruvhacks <sridhruv@yahoo.com>
2016-09-02 20:44:12 +0530

    Iterations Increased

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "attendance.class"
 #include <cstdio>
 using namespace std;
@@ -58,7 +57,6 @@ int main()
 			default:
 			{	printf("Incorrect input.\n\n");	break;}
 		}
-
 		cout<<"\n Want to do something more??(Y/N) \n";
 		scanf("%sc",&choice);
 		if(choice=='y'||choice=='Y')
@@ -69,75 +67,4 @@ int main()
 			{	printf("Incorrect input. Run_Aborted.\n\n");	break;	}
 	}
 	return 0;
-=======
-#include "attendance.class"
-#include <cstdio>
-using namespace std;
-
-int main()
-{
-	attendance ob;
-	while(1)
-	{
-		char choice;
-		ob.begin();
-		scanf("%s",&choice);
-		switch(choice)
-		{
-			case '1':
-			{	
-				ob.day();
-				ob.task();
-				ob.show();
-				printf("Any updates? (Y/N)\n");
-				scanf("%s",&choice);
-				if(choice=='y'||choice=='Y')
-					ob.update();
-				else if(choice=='n'||choice=='N')
-					break; 
-				else
-					printf("Incorrect input. \n\n");
-			}
-			case '2':	
-			{	
-				ob.show();
-				ob.end();
-				break; 
-			}
-			case '3':
-			{ 
-				ob.update();
-				ob.end();
-				break;
-			}
-			case '4':
-			{
-				ob.newm();
-				printf("\n\n");
-				ob.show();
-				ob.end();
-				break;
-			}
-			case '5':
-			{
-				printf("\n");
-				ob.caditshow();
-				ob.end();
-				break;
-			}
-			default:
-			{	printf("Incorrect input.\n\n");	break;}
-		}
-
-		cout<<"\n Want to do something more??(Y/N) \n";
-		scanf("%sc",&choice);
-		if(choice=='y'||choice=='Y')
-			continue;
-		else if(choice=='n'||choice=='N')
-			{ ob.end();	break;	}
-		else
-			{	printf("Incorrect input. Run_Aborted.\n\n");	break;	}
-	}
-	return 0;
->>>>>>> a534c7d6a11cb9951e12ec8de3e04b47208b4eed
 }
